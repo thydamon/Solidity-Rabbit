9a34a03838f4c709562276cccbfba74f9b9f354c
tanghao27478 <tanghao27478@hundsun.com>
2025-07-07 20:38:16 +0800

    feat: 新增Add.sol合约

---
+++ hardhat-tutorial/contracts/Add.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+// Hardhat 中文网: http://hardhat.cn
+pragma solidity ^0.8.20;
+
+contract Add {
+    function add(uint a, uint b) public pure returns(uint) {
+        return a+b;
+    }
+}
