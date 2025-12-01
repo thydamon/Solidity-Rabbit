8fb9692276d0c53f1d2b72e39aff9d841ddbe2da
thydamon@gmail.com <tanghao27478@hundsun.com>
2025-12-01 13:36:01 +0800

    feat:部署到sepolia

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,7 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.ETH_SEPOILA_URL + process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
 };
